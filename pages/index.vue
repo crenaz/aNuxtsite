f6748aca2d8a9bca7eb1aee88118d2a5efa20add
crenaz <crenaz@gmail.com>
2022-11-24 10:26:04 -0400

    Adjusted the vue index file

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
+
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to this example app made with Vuetify + Nuxt.js
+        </v-card-title>
+
+        <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
         <VuetifyLogo />
       </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
+
         <v-card-text>
           <p>
             Vuetify is a progressive Material Design component framework for
